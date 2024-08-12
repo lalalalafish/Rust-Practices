6948bed56538e9b1db4a9458a763d4556537bacd
lalalalafish <liu619666@163.com>
2024-08-12 15:27:41 +0800

    first

---
+++ hello_world/target/debug/deps/hello_world-451dd4f7492dae29.d
@@ -0,0 +1,5 @@
+/home/haha/Rust/hello_world/target/debug/deps/hello_world-451dd4f7492dae29: src/main.rs
+
+/home/haha/Rust/hello_world/target/debug/deps/hello_world-451dd4f7492dae29.d: src/main.rs
+
+src/main.rs:
